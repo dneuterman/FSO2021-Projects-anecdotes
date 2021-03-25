88e34547413fb0728ea09b6e51a89b069c32ab92
Donald Neuterman <dneuterman@gmail.com>
2021-03-24 23:54:06 -0400

    project anecdotes complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const Button = (props) => (
+  <button onClick={props.handleClick}>{props.text}</button>
+)
+
+const Anecdote = ({anecdote, vote}) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <p>{anecdote}</p>
+    <p>Vote Count: {vote}</p>
+    </>
+  )
 }
 
-export default App;
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  ]
+
+  const [votes, setVotes] = useState(new Uint8Array(anecdotes.length))
+  const [selected, setSelected] = useState(0)
+  const [mostVotes, setMostVotes] = useState(0)
+
+  console.log(votes)
+
+  const selectQuote = () => {
+    let randNum = Math.floor(Math.random() * anecdotes.length)
+    setSelected(randNum)
+  }
+
+  const voteForQuote = () => {
+    const newVotes = [...votes]
+    newVotes[selected] += 1
+    setVotes(newVotes)
+    findMostVotes()
+  }
+
+  const findMostVotes = () => {
+    let maxNumber = 0
+    let voteIndex = 0
+    console.log(mostVotes)
+    for (let i=0; i < votes.length; i++) {
+      if (votes[i] >= maxNumber) {
+        maxNumber = votes[i]
+        voteIndex = i
+      }
+    }
+    setMostVotes(voteIndex)
+  }
+
+  return (
+    <div>
+      <h2>Anecdote of the Day</h2>
+      <Anecdote anecdote={anecdotes[selected]} vote={votes[selected]}/>
+      <Button handleClick={voteForQuote} text="Vote" />
+      <Button handleClick={selectQuote} text="Random Anecdote" />
+      <h2>Anecdote With Most Votes</h2>
+      <Anecdote anecdote={anecdotes[mostVotes]} vote={votes[mostVotes]}/>
+    </div>
+  )
+}
+
+export default App
